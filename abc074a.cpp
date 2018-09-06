e8e414ddd11d3ea18573fcc6f9dc259dbe2b397d
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-09-06 23:56:56 +0900

    abc074a

---
+++ abc074a.cpp
@@ -0,0 +1,17 @@
+// https://beta.atcoder.jp/contests/abc074
+// status:
+// tag:
+
+#define SUBMIT
+//#define DEBUG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, A;
+    cin >> N >> A;
+    auto ans = N * N - A;
+    cout << ans << endl;
+    return 0;
+}
