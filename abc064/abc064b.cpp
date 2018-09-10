7abbc22e20e8aca30739b95d63b3503a930c869b
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-09-11 00:29:02 +0900

    abc064b

--- abc064/abc064b.cpp
+++ abc064/abc064b.cpp
@@ -3,11 +3,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_yes_no(bool cond, string yes = "Yes", string no = "No") { cout << (cond ? yes : no) << endl; }
-
 int main() {
-    string s;
-    cin >> s;
-    print_yes_no(s[0] == s[2]);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    sort(a.begin(), a.end());
+    auto ans = *max_element(a.begin(), a.end()) - *min_element(a.begin(), a.end());
+    cout << ans << endl;
     return 0;
 }
