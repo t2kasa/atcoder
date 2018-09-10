fe81fad1b75517bf40a88210e1dbe570f96641f0
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-09-11 00:19:37 +0900

    abc064c

--- abc064/abc064c.cpp
+++ abc064/abc064c.cpp
@@ -1,13 +1,35 @@
-// https://beta.atcoder.jp/contests/abc070
+// https://beta.atcoder.jp/contests/abc064
 
+#define SUBMIT
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_yes_no(bool cond, string yes = "Yes", string no = "No") { cout << (cond ? yes : no) << endl; }
-
 int main() {
-    string s;
-    cin >> s;
-    print_yes_no(s[0] == s[2]);
+#ifdef SUBMIT
+    auto& stream = cin;
+#else
+    stringstream stream(R"(9
+3200 3200 1600 2000 3200 3200 3200 3200 3200
+)");
+#endif
+
+    int count8 = 0;
+    // (color, count)
+    set<int> color_counts;
+    int n;
+    stream >> n;
+    for (int i = 0; i < n; ++i) {
+        int a;
+        stream >> a;
+        a = min(3200, a);
+        a /= 400;
+        color_counts.insert(a);
+        if(a == 8) count8++;
+    }
+
+    int ans_min = color_counts.size() - (0 < count8);
+    if (ans_min == 0) ans_min = 1;
+    int ans_max = color_counts.size() - (0 < count8) + count8;
+    cout << ans_min << " " << ans_max << endl;
     return 0;
 }
