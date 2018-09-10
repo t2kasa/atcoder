7731d4662821a8d88d2682206287008f60c55c22
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-09-11 00:30:47 +0900

    abc064a

--- abc064/abc064a.cpp
+++ abc064/abc064a.cpp
@@ -3,11 +3,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_yes_no(bool cond, string yes = "Yes", string no = "No") { cout << (cond ? yes : no) << endl; }
+void print_yes_no(bool cond, string yes = "YES", string no = "NO") { cout << (cond ? yes : no) << endl; }
 
 int main() {
-    string s;
-    cin >> s;
-    print_yes_no(s[0] == s[2]);
+    int r, g, b;
+    cin >> r >> g >> b;
+    print_yes_no((100 * r + 10 * g + b) % 4 == 0);
     return 0;
 }
