c9a61399f71725539cbdf132f58a655a11e1c704
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-09-11 20:41:01 +0900

    abc069b

---
+++ abc069b.cpp
@@ -0,0 +1,11 @@
+// https://beta.atcoder.jp/contests/abc069
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s[0] << s.length() - 2 << s[s.length() - 1] << endl;
+    return 0;
+}
