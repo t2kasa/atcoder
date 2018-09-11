526caf01137e2ec95c1b56ce5756fe7dfc8ea341
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-09-11 20:36:54 +0900

    abc068a

---
+++ abc068a.cpp
@@ -0,0 +1,11 @@
+// https://beta.atcoder.jp/contests/abc068
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << "ABC" << s << endl;
+    return 0;
+}
